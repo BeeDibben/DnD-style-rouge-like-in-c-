06714a89c8901f3e6f2f3c2cb904068351c255a9
Bee Dibben <95137814+BeeDibben@users.noreply.github.com>
2023-11-17 15:05:29 +0000

    Outlined location class and subclasses

--- DnD style rouge-like in c++/DnD style rouge-like in c++.cpp
+++ DnD style rouge-like in c++/DnD style rouge-like in c++.cpp
@@ -2,8 +2,46 @@
 //
 
 #include <iostream>
+#include <location.h>
+
+bool gameLoop = true;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    srand(time(0));
+    
+	initialiseGameComponents();
+
+	while (gameLoop) {
+
+	}
+	// intro
+		// Start menu
+			// Build character
+		// Game logic
+			// Setting
+			// Quest prompts
+			// Map additions (Map locations & sublocations)
+			// What would you like to do/where would you like to go
+				// Go to a new Place
+					// Return to setting
+				//Engage in quest
+					// Attempt quest -- success
+						// Return to the settlemt
+						// Claim reward
+						// Go to a new place
+						// return to setting
+					// Attempt quest -- Fail
+						// Return to the settlement
+						// go to a new place 
+						// return to setting
+
+}
+
+void initialiseGameComponents() {
+
+}
+
+void() {
+
 }
